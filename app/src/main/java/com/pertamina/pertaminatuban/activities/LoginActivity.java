ac39ebd8d63d5e990e52dfdb185d6056ea91096e
Udine <udines@users.noreply.github.com>
2018-01-17 16:54:14 +0700

    Update Login
    Request http api login berhasil.
    Tambah logo pertamina.
    Ganti nama aplikasi menjadi Click Tuban

--- app/src/main/java/com/pertamina/pertaminatuban/activities/LoginActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/activities/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.pertamina.pertaminatuban.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pertamina.pertaminatuban.R;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,9 +33,34 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
         /*mengurus pengambilan data username dan password lalu melakukan
         * otentikasi menggunakan data diatas*/
         handleLogin();
+
+        /*respons pengguna ketika klik daftar*/
+        handleRegister();
+    }
+
+    private void handleRegister() {
+
+        /*menghubungkan variabel view dengan layout*/
+        TextView registerButton = findViewById(R.id.login_daftar);
+
+        /*eksekusi instruksi di bawah ketika pengguna klik tombol daftar*/
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*masuk ke halaman pendaftaran*/
+                Intent registerIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                registerIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(registerIntent);
+            }
+        });
     }
 
     private void handleLogin() {
@@ -67,12 +103,54 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void otentikasi(String username, String password) {
-        Toast.makeText(this, username + " " + password, Toast.LENGTH_SHORT).show();
-        Intent menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
-        menuIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(menuIntent);
+    private void otentikasi(String username, String password)  {
+        sendPost(username, password);
     }
 
+    /**/
+    private void sendPost(final String username, final String password) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
+                    /*api url untuk login*/
+                    URL url = new URL("http://www.api.odc-abcd.com/login");
+
+                    /*membuat koneksi dan set metode POST*/
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+
+                    /*membuat obyek json yang berisi data login*/
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("username", username);
+                    jsonObject.put("password", password);
+                    Log.d("json", jsonObject.toString());
+
+                    /*mengubah json menjadi output stream*/
+                    DataOutputStream os = new DataOutputStream(connection.getOutputStream());
+                    os.writeBytes(jsonObject.toString());
+                    os.flush();
+                    os.close();
+
+                    Log.d("connection", String.valueOf(connection.getResponseCode()));
+                    Log.d("connection", String.valueOf(connection.getResponseMessage()));
+
+                    /*Log hasil request body*/
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        Log.d("connection body", br.lines().collect(Collectors.joining()));
+                    }
+
+                    connection.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
 }
