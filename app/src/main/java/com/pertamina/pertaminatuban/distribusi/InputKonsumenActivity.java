c5cff7d6b5cdfbd57f34cdea47bf9bee16c09151
Udine <udines@users.noreply.github.com>
2018-01-23 15:15:41 +0700

    Tambah Method POST
    tambah method untuk send data arraylist ke server di halaman input konsumen, opers, ritase, dan wilayah.

--- app/src/main/java/com/pertamina/pertaminatuban/distribusi/InputKonsumenActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/distribusi/InputKonsumenActivity.java
@@ -1,13 +1,33 @@
 package com.pertamina.pertaminatuban.distribusi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.pertamina.pertaminatuban.R;
+import com.pertamina.pertaminatuban.distribusi.models.Konsumen;
+import com.pertamina.pertaminatuban.distribusi.models.Ritase;
+import com.pertamina.pertaminatuban.service.UserClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InputKonsumenActivity extends AppCompatActivity {
 
@@ -26,4 +46,60 @@ public class InputKonsumenActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void sendPostRequest(ArrayList<Konsumen> konsumens) {
+        Log.w("input size", String.valueOf(konsumens.size()));
+
+        SharedPreferences preferences = InputKonsumenActivity.this.getSharedPreferences(
+                "login",
+                Context.MODE_PRIVATE
+        );
+
+        final String key = preferences.getString("userKey", "none");
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request request = original.newBuilder()
+                        .header("Authorization", key)
+                        .method(original.method(), original.body())
+                        .build();
+                return chain.proceed(request);
+            }
+        });
+
+        String json = new Gson().toJson(konsumens);
+        Log.w("json", json);
+
+        OkHttpClient client = httpClient.build();
+
+        String baseUrl = "http://www.api.clicktuban.com/";
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client);
+
+        Retrofit retrofit = builder.build();
+        UserClient userClient = retrofit.create(UserClient.class);
+
+        Call<Object> call = userClient.postKonsumen(konsumens);
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+                Log.w("code", String.valueOf(response.code()));
+                if (response.code() == 200) {
+                    Log.w("response ", response.body().toString());
+                    Toast.makeText(InputKonsumenActivity.this, "Data berhasil ditambahkan", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+                Log.w("error", t.getMessage());
+            }
+        });
+    }
 }
