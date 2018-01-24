7bf8e67f1fada9cb926e2cdfda5d796b7dea8929
Fata <sendtofata@live.com>
2018-01-24 10:18:12 +0700

    Update Input Ritase
    tambah method untuk menghandle date picker.

--- app/src/main/java/com/pertamina/pertaminatuban/distribusi/InputRitaseActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/distribusi/InputRitaseActivity.java
@@ -1,15 +1,26 @@
 package com.pertamina.pertaminatuban.distribusi;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.pertamina.pertaminatuban.R;
 
+import java.util.Calendar;
+
 public class InputRitaseActivity extends AppCompatActivity {
+
+    private int year, month, day;
+    private boolean tanggalSet;
+    private Button tanggal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,56 @@ public class InputRitaseActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+         /*inisialisasi view yang digunakan lebih dari satu fungsi*/
+        tanggal = findViewById(R.id.input_ritase_tanggal);
+
+        /*init tanggal*/
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        handleDatePicker();
+    }
+
+    private boolean isDateSet() {
+        if (!tanggalSet) {
+            Toast.makeText(this, "Pilih tanggal", Toast.LENGTH_SHORT).show();
+        }
+        return tanggalSet;
+    }
+
+    private void setDateButton(int year, int month, int day) {
+        tanggal.setText(String.valueOf(year + " - " + month + 1 + " - " + day));
+    }
+
+    private void handleDatePicker() {
+        final DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                year = i;
+                month = i1;
+                day = i2;
+                setDateButton(year, month, day);
+                tanggalSet = true;
+            }
+        };
+
+        tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("button", "date button clicked");
+                DatePickerDialog dialog = new DatePickerDialog(
+                        InputRitaseActivity.this,
+                        listener,
+                        year,
+                        month,
+                        day
+                );
+                dialog.show();
+            }
+        });
     }
 
 }
