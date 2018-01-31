c8816c20e672736506c161a47f387a979b262652
Fata <sendtofata@live.com>
2018-01-31 23:31:45 +0700

    Update Input Port Charges
    tambah layout edittext dan findviewbyid

--- app/src/main/java/com/pertamina/pertaminatuban/marine/InputPortChargesActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/InputPortChargesActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pertamina.pertaminatuban.R;
 
@@ -24,6 +26,34 @@ public class InputPortChargesActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getData();
+    }
+
+    private void getData() {
+        /*variable view*/
+        EditText inputRambu, inputLabuh, inputTambat, inputPandu, inputTunda, inputPup;
+        Button kirim;
+
+        /*hubungkan variable view dengan layout*/
+        inputRambu = findViewById(R.id.input_port_charges_rambu);
+        inputLabuh = findViewById(R.id.input_port_charges_labuh);
+        inputTambat = findViewById(R.id.input_port_charges_tambat);
+        inputPandu = findViewById(R.id.input_port_charges_pandu);
+        inputTunda = findViewById(R.id.input_port_charges_tunda);
+        inputPup = findViewById(R.id.input_port_charges_pup);
+        kirim = findViewById(R.id.input_port_charges_kirim);
+
+        /*variable data*/
+        long rambu, labuh, tambat, pandu, tunda, pup;
+
+        /*handle pengguna klik button kirim*/
+        kirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 
 }
