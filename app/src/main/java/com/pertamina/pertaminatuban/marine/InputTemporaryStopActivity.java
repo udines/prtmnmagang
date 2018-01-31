12fcd5db0e4063adb7a68d26345068bdffc0adb2
Fata <sendtofata@live.com>
2018-01-31 23:59:17 +0700

    Update Temporary Stop
    tambah view layout dan findviewbyid

--- app/src/main/java/com/pertamina/pertaminatuban/marine/InputTemporaryStopActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/InputTemporaryStopActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pertamina.pertaminatuban.R;
 
@@ -24,6 +26,20 @@ public class InputTemporaryStopActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getData();
+    }
+
+    private void getData() {
+        EditText inputReason;
+        Button inputStopDate, inputStopTime, inputResumeDate, inputResumeTime, kirim;
+
+        inputReason = findViewById(R.id.input_temporary_stop_reason);
+        inputStopDate = findViewById(R.id.input_temporary_stop_date_stop);
+        inputStopTime = findViewById(R.id.input_temporary_stop_time_stop);
+        inputResumeDate = findViewById(R.id.input_temporary_stop_date_resume);
+        inputResumeTime = findViewById(R.id.input_temporary_stop_time_resume);
+        kirim = findViewById(R.id.input_temporary_stop_kirim);
     }
 
 }
