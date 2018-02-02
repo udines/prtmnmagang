2f5e96ffb2b9869838c0b2014af2d9fc38ab31bc
Udine <udines@users.noreply.github.com>
2018-02-03 02:41:27 +0700

    Tambah Model Initial Tanker

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/InitialTanker.java
@@ -0,0 +1,24 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+import java.sql.Date;
+
+public class InitialTanker {
+    private String id;
+    private int call;
+    private Date period;
+    private String voyage;
+    private String noBill;
+    private Date dateOfBill;
+    private String status;
+    private String grades;
+    private String handlingAgent;
+    private String generalAgent;
+    private String cargoStatus;
+    private String tankerActivity;
+    private String pumpMethod;
+    private String barthing;
+    private String portOfCall;
+    private String portOfCallReport;
+    private String lastPort;
+    private String nextPort;
+}
