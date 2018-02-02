0b5eb26dea2d033844c7153463e7f287713ad79b
Udine <udines@users.noreply.github.com>
2018-02-02 19:54:27 +0700

    Update Input Ship Particular
    tambah layout view dan findviewbyid

--- app/src/main/java/com/pertamina/pertaminatuban/marine/InputShipParticularActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/InputShipParticularActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pertamina.pertaminatuban.R;
 
@@ -24,6 +26,23 @@ public class InputShipParticularActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getData();
+    }
+
+    private void getData() {
+        EditText inputFlag, inputDwt, inputGrt, inputLoa, inputTypeCall, inputTypeActivity, inputRate,
+                inputMaster;
+        Button kirim;
+        inputFlag = findViewById(R.id.input_ship_particular_flag);
+        inputDwt = findViewById(R.id.input_ship_particular_dwt);
+        inputGrt = findViewById(R.id.input_ship_particular_grt);
+        inputLoa = findViewById(R.id.input_ship_particular_loa);
+        inputTypeCall = findViewById(R.id.input_ship_particular_type_call);
+        inputTypeActivity = findViewById(R.id.input_ship_particular_type_activity);
+        inputRate = findViewById(R.id.input_ship_particular_rate);
+        inputMaster = findViewById(R.id.input_ship_particular_master);
+        kirim = findViewById(R.id.input_ship_particular_kirim);
     }
 
 }
