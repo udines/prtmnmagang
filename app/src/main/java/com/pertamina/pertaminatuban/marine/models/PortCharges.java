2ab1f50a925d2cb72e9d11096efcc05ff8dbe235
Udine <udines@users.noreply.github.com>
2018-02-03 12:26:23 +0700

    Tambah Model Port Charges

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/PortCharges.java
@@ -0,0 +1,13 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+public class PortCharges {
+    private String id;
+    private String foreignKey;
+
+    private float lightDues;
+    private float harborDues;
+    private float quayDues;
+    private float pilotage;
+    private float towage;
+    private float pup9a2;
+}
