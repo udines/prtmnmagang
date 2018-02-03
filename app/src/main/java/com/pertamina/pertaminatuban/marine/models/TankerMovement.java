a00ad9772044931ee4bf0396a8ce5fd79709e940
Udine <udines@users.noreply.github.com>
2018-02-03 12:05:56 +0700

    Tambah Model Tanker Movement

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/TankerMovement.java
@@ -0,0 +1,32 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+import java.sql.Timestamp;
+
+public class TankerMovement {
+    private Timestamp allFast;
+    private Timestamp channelConnection;
+    private Timestamp dryCertifIssued1;
+    private Timestamp completedCargoCalculation1;
+    private Timestamp labTestReleased1;
+    private Timestamp commenceDisLoad;
+    private Timestamp completedDisLoad;
+    private Timestamp completedHoseDis;
+    private Timestamp dryCertifIssued2;
+    private Timestamp completedCargoCalculation2;
+    private Timestamp labTestReleased2;
+    private Timestamp cargoDocument;
+    private Timestamp PortClearence;
+    private Timestamp bookingPilotUnberthing;
+    private Timestamp pilotOnBoardUnberthing;
+    private Timestamp castOff;
+    private Timestamp anchoredRede;
+    private Timestamp pilotOnBoardDeparture;
+    private Timestamp anchorDeparture;
+    private Timestamp actualTimeDeparture;
+    private Timestamp delivery;
+    private Timestamp redelivery;
+    private Timestamp onHire;
+    private Timestamp offHire;
+    private Timestamp offToOn;
+    private String remarksActivity;
+}
