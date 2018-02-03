46f0fe542ae16773ed0df3639010378b5a1c31df
Udine <udines@users.noreply.github.com>
2018-02-03 12:49:57 +0700

    Tambah Model Waiting Time

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/WaitingTime.java
@@ -0,0 +1,33 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+public class WaitingTime {
+    private String id;
+    private String foreignKey;
+
+    private int pilot;
+    private int labAnalysis;
+    private int tubBoat;
+    private int jetty;
+    private int daylight;
+    private int tide;
+    private int ballast;
+    private int tankCleaning;
+    private int nomination;
+    private int manPower;
+    private int badWeater;
+    private int line;
+    private int cargo;
+    private int ullage;
+    private int supplyBunker;
+    private int supplyFreshWater;
+    private int actLoadDate;
+    private int preparation;
+    private int shoreOrder;
+    private int shipClearence;
+    private int cargoDocument;
+    private int slowPumpVessel;
+    private int slowPumpShore;
+    private int cargoCalculation;
+    private int steamingInOut;
+    private int shipUnready;
+}
