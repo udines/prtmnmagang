50bf1a6685d264a7ff73cd0bce25a2c0d40e4cc6
Udine <udines@users.noreply.github.com>
2018-02-03 12:18:46 +0700

    Tambah Model Ship Condition

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/ShipCondition.java
@@ -0,0 +1,21 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+import java.sql.Timestamp;
+
+public class ShipCondition {
+    private String descriptionDraft;
+    private Timestamp actualTimeArrival;
+    private Timestamp actualTimeDeparture;
+    private String gradeBunker;
+    private float robLastPort;
+    private float robAta;
+    private float robAtd;
+    private float repl;
+    private Timestamp comRepl;
+    private Timestamp compRepl;
+    private String replLocation;
+    private float bunkerConsumptionPort;
+    private float bunkerConsumptionSeatime;
+    private float slopTankAta;
+    private float slopTankAtd;
+}
