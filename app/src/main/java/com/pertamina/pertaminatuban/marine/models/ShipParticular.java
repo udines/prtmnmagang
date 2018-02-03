2b960fbcdba5cbdf67f0f89957e8e9105efd35de
Udine <udines@users.noreply.github.com>
2018-02-03 12:35:31 +0700

    Tambah Model Ship Particular

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/ShipParticular.java
@@ -0,0 +1,15 @@
+package com.pertamina.pertaminatuban.marine.models;
+
+public class ShipParticular {
+    private String id;
+    private String foreignKey;
+
+    private String flag;
+    private float dwt;
+    private float grt;
+    private float loa;
+    private String typeCall;
+    private String typeActivity;
+    private float hireRate;
+    private String master;
+}
