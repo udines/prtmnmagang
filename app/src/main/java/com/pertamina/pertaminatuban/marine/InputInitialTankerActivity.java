c4d2a7dbdf9162243900a7844ab62d6d76e77d71
Fata <sendtofata@live.com>
2018-02-06 20:18:37 +0700

    Update Input Initial Tanker
    set data awalan input jika data sudah ada.

--- app/src/main/java/com/pertamina/pertaminatuban/marine/InputInitialTankerActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/InputInitialTankerActivity.java
@@ -1,18 +1,35 @@
 package com.pertamina.pertaminatuban.marine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.pertamina.pertaminatuban.R;
+import com.pertamina.pertaminatuban.marine.models.InitialTanker;
+
+import java.sql.Date;
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class InputInitialTankerActivity extends AppCompatActivity {
+
+    private InitialTanker initialTanker;
+
+    private EditText inputCallTanker, inputVoyage, inputNoBill, inputHandlingAgent, inputGeneralAgent,
+            inputCargoStatus;
+    private Button inputPeriode, kirim;
+    private RadioGroup groupStatusTanker, groupStatusOperasional, groupGrades, groupTankerActivity,
+            groupPumpingMethod, groupBarthing, groupPortCall, groupPortCallReport, groupLastPort,
+            groupNextPort;
+    private List<String> portChoice, barthingChoice, statusChoice, activityChoice, methodChoice, gradeChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +45,42 @@ public class InputInitialTankerActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        initChoices();
         getData();
     }
 
+    private void initChoices() {
+        portChoice = new ArrayList<>();
+        portChoice.add(getResources().getString(R.string.radio_loading_port));
+        portChoice.add(getResources().getString(R.string.radio_discharge_port));
+        Log.w("port choice size", String.valueOf(portChoice.size()));
+
+        barthingChoice = new ArrayList<>();
+        barthingChoice.add(getResources().getString(R.string.radio_spm_35));
+        barthingChoice.add(getResources().getString(R.string.radio_spm_150));
+        barthingChoice.add(getResources().getString(R.string.radio_jetty_tppi_2));
+        barthingChoice.add(getResources().getString(R.string.radio_jetty_tppi_3));
+        Log.w("barthing choice size", String.valueOf(barthingChoice.size()));
+
+        statusChoice = new ArrayList<>();
+        statusChoice.add(getResources().getString(R.string.radio_own_tanker));
+        statusChoice.add(getResources().getString(R.string.radio_charter_tanker));
+
+        activityChoice = new ArrayList<>();
+        activityChoice.add(getResources().getString(R.string.radio_discharge));
+        activityChoice.add(getResources().getString(R.string.radio_loading));
+
+        methodChoice = new ArrayList<>();
+        methodChoice.add(getResources().getString(R.string.radio_grade_by_grade));
+        methodChoice.add(getResources().getString(R.string.radio_simultan));
+
+        gradeChoice = new ArrayList<>();
+        gradeChoice.add(getResources().getString(R.string.text_pertamax));
+        gradeChoice.add(getResources().getString(R.string.text_premium));
+        gradeChoice.add(getResources().getString(R.string.text_solar));
+    }
+
     private void getData() {
-        EditText inputCallTanker, inputVoyage, inputNoBill, inputHandlingAgent, inputGeneralAgent,
-                inputCargoStatus;
-        Button inputPeriode, kirim;
-        RadioGroup groupStatusTanker, groupStatusOperasional, groupGrades, groupTankerActivity,
-                groupPumpingMethod, groupBarthing, groupPortCall, groupPortCallReport, groupLastPort,
-                groupNextPort;
 
         inputCallTanker = findViewById(R.id.input_initial_tanker_call_tanker);
         inputVoyage = findViewById(R.id.input_initial_tanker_voyage_tanker);
@@ -59,5 +102,108 @@ public class InputInitialTankerActivity extends AppCompatActivity {
         groupTankerActivity = findViewById(R.id.input_initial_tanker_group_tanker_activity);
         groupPumpingMethod = findViewById(R.id.input_initial_tanker_group_pumpung_method);
         groupBarthing = findViewById(R.id.input_initial_tanker_group_barthing);
+
+        if (currentDataExist()) {
+            getInitialData();
+            setInitialData();
+        }
+
+        kirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInputData();
+            }
+        });
+    }
+
+    private void getInputData() {
+
+    }
+
+    private void setInitialData() {
+
+        setEditText(inputCallTanker, String.valueOf(initialTanker.getCall()));
+        setEditText(inputVoyage, initialTanker.getVoyage());
+        setEditText(inputNoBill, initialTanker.getNoBill());
+        setEditText(inputHandlingAgent, initialTanker.getHandlingAgent());
+        setEditText(inputGeneralAgent, initialTanker.getGeneralAgent());
+        setEditText(inputCargoStatus, initialTanker.getCargoStatus());
+
+        setDateButtonText(inputPeriode, initialTanker.getPeriod());
+
+        setRadioSelected(groupPortCall, portChoice, initialTanker.getPortOfCall());
+        setRadioSelected(groupPortCallReport, portChoice, initialTanker.getPortOfCallReport());
+        setRadioSelected(groupLastPort, portChoice, initialTanker.getLastPort());
+        setRadioSelected(groupNextPort, portChoice, initialTanker.getNextPort());
+
+        setRadioSelected(groupBarthing, barthingChoice, initialTanker.getBarthing());
+
+        setRadioSelected(groupStatusTanker, statusChoice, initialTanker.getStatus());
+        setRadioSelected(groupStatusOperasional, statusChoice, initialTanker.getStatusOps());
+
+        setRadioSelected(groupTankerActivity, activityChoice, initialTanker.getTankerActivity());
+        setRadioSelected(groupPumpingMethod, methodChoice, initialTanker.getPumpMethod());
+        setRadioSelected(groupGrades, gradeChoice, initialTanker.getGrades());
+    }
+
+    private void setRadioSelected(RadioGroup group, List<String> choices, String chosen) {
+        if (chosen != null && !chosen.isEmpty()) {
+            Log.w("chosen", chosen);
+            for (int i = 0; i < choices.size(); i++) {
+                if (choices.get(i).equals(chosen)) {
+                    Log.w("index", String.valueOf(i));
+                    int id = group.getChildAt(i).getId();
+                    group.check(id);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void setEditText(EditText editText, String content) {
+        if (content != null && !content.isEmpty()) {
+            editText.setText(content);
+        }
+    }
+
+    private void setDateButtonText(Button dateButton, Date date) {
+        if (date != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(date.getTime());
+            DateFormatSymbols symbols = new DateFormatSymbols();
+            String text = symbols.getMonths()[cal.get(Calendar.MONTH)] + " " + String.valueOf(cal.get(Calendar.YEAR));
+            dateButton.setText(text);
+        }
+    }
+
+    private boolean currentDataExist() {
+        return true;
+    }
+
+    private void getInitialData() {
+
+        Calendar cal = Calendar.getInstance();
+
+        initialTanker = new InitialTanker(
+                "idAbc123",
+                1,
+                new Date(cal.getTimeInMillis()),
+                "15/D1/P.3010/VII/2017",
+                "12",
+                new Date(cal.getTimeInMillis()),
+                "Own Tanker",
+                "Own Tanker",
+                "",
+                "PTM",
+                "PTM",
+                "Domestic",
+                null,
+                "Simultan",
+                getResources().getString(R.string.radio_spm_35),
+                "Discharge Port",
+                "Discharge Port",
+                "Loading Port",
+                "Discharge Port"
+        );
     }
 }
--- app/src/main/java/com/pertamina/pertaminatuban/marine/models/InitialTanker.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/InitialTanker.java
@@ -10,6 +10,7 @@ public class InitialTanker {
     private String noBill;
     private Date dateOfBill;
     private String status;
+    private String statusOps;
     private String grades;
     private String handlingAgent;
     private String generalAgent;
@@ -21,4 +22,120 @@ public class InitialTanker {
     private String portOfCallReport;
     private String lastPort;
     private String nextPort;
+
+    public InitialTanker(String id,
+                         int call,
+                         Date period,
+                         String voyage,
+                         String noBill,
+                         Date dateOfBill,
+                         String status,
+                         String statusOps,
+                         String grades,
+                         String handlingAgent,
+                         String generalAgent,
+                         String cargoStatus,
+                         String tankerActivity,
+                         String pumpMethod,
+                         String barthing,
+                         String portOfCall,
+                         String portOfCallReport,
+                         String lastPort,
+                         String nextPort) {
+        this.id = id;
+        this.call = call;
+        this.period = period;
+        this.voyage = voyage;
+        this.noBill = noBill;
+        this.dateOfBill = dateOfBill;
+        this.status = status;
+        this.statusOps = statusOps;
+        this.grades = grades;
+        this.handlingAgent = handlingAgent;
+        this.generalAgent = generalAgent;
+        this.cargoStatus = cargoStatus;
+        this.tankerActivity = tankerActivity;
+        this.pumpMethod = pumpMethod;
+        this.barthing = barthing;
+        this.portOfCall = portOfCall;
+        this.portOfCallReport = portOfCallReport;
+        this.lastPort = lastPort;
+        this.nextPort = nextPort;
+    }
+
+    public String getStatusOps() {
+        return statusOps;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getCall() {
+        return call;
+    }
+
+    public Date getPeriod() {
+        return period;
+    }
+
+    public String getVoyage() {
+        return voyage;
+    }
+
+    public String getNoBill() {
+        return noBill;
+    }
+
+    public Date getDateOfBill() {
+        return dateOfBill;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getGrades() {
+        return grades;
+    }
+
+    public String getHandlingAgent() {
+        return handlingAgent;
+    }
+
+    public String getGeneralAgent() {
+        return generalAgent;
+    }
+
+    public String getCargoStatus() {
+        return cargoStatus;
+    }
+
+    public String getTankerActivity() {
+        return tankerActivity;
+    }
+
+    public String getPumpMethod() {
+        return pumpMethod;
+    }
+
+    public String getBarthing() {
+        return barthing;
+    }
+
+    public String getPortOfCall() {
+        return portOfCall;
+    }
+
+    public String getPortOfCallReport() {
+        return portOfCallReport;
+    }
+
+    public String getLastPort() {
+        return lastPort;
+    }
+
+    public String getNextPort() {
+        return nextPort;
+    }
 }
