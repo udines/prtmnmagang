18580cce13bc565aa36142ad4ec9186b429d4b6b
Fata <sendtofata@live.com>
2018-02-06 21:50:17 +0700

    Update Input Ship Condition
    set initial input data jika data ada

--- app/src/main/java/com/pertamina/pertaminatuban/marine/InputShipConditionActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/InputShipConditionActivity.java
@@ -1,17 +1,31 @@
 package com.pertamina.pertaminatuban.marine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.pertamina.pertaminatuban.R;
+import com.pertamina.pertaminatuban.marine.models.ShipCondition;
+
+import java.sql.Timestamp;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class InputShipConditionActivity extends AppCompatActivity {
+
+    private EditText inputDescription, inputGradesBunker, inputRobLastPort, inputRobAta, inputRobAtd,
+            inputRepl, inputLocationRepl, inputBunkerConsPort, inputBunkerConsSeatime,
+            inputSlopTankAta, inputSlopTankAtd;
+    private Button ataTime, atdTime, comReplDate, comReplTime, compReplDate, compReplTime, kirim;
+
+    private ShipCondition condition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +41,10 @@ public class InputShipConditionActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getData();
+        initData();
     }
 
-    private void getData() {
-        EditText inputDescription, inputGradesBunker, inputRobLastPort, inputRobAta, inputRobAtd,
-                inputRepl, inputLocationRepl, inputBunkerConsPort, inputBunkerConsSeatime,
-                inputSlopTankAta, inputSlopTankAtd;
-        Button ataTime, atdTime, comReplDate, comReplTime, compReplDate, compReplTime, kirim;
+    private void initData() {
 
         inputDescription = findViewById(R.id.input_ship_condition_description_draft);
         inputGradesBunker = findViewById(R.id.input_ship_condition_grades_bunker);
@@ -55,6 +65,85 @@ public class InputShipConditionActivity extends AppCompatActivity {
         compReplDate = findViewById(R.id.input_ship_condition_comp_repl_date);
         compReplTime = findViewById(R.id.input_ship_condition_comp_repl_time);
         kirim = findViewById(R.id.input_ship_condition_kirim);
+
+        if (currentDataExist()) {
+            getCurrentData();
+            setInitialInput();
+        }
     }
 
+    private void setInitialInput() {
+        setDateAndTimeButton(null, ataTime, condition.getActualTimeArrival());
+        setDateAndTimeButton(null, atdTime, condition.getActualTimeDeparture());
+        setDateAndTimeButton(compReplDate, compReplTime, condition.getCompRepl());
+        setDateAndTimeButton(comReplDate, comReplTime, condition.getComRepl());
+
+        setEditText(inputDescription, condition.getDescriptionDraft());
+        setEditText(inputGradesBunker, condition.getGradeBunker());
+        setEditText(inputRobAta, condition.getRobAta());
+        setEditText(inputRobAtd, condition.getRobAtd());
+        setEditText(inputRobLastPort, condition.getRobLastPort());
+        setEditText(inputRepl, condition.getRepl());
+        setEditText(inputLocationRepl, condition.getReplLocation());
+        setEditText(inputBunkerConsPort, condition.getBunkerConsumptionPort());
+        setEditText(inputBunkerConsSeatime, condition.getBunkerConsumptionSeatime());
+        setEditText(inputSlopTankAta, condition.getSlopTankAta());
+        setEditText(inputSlopTankAtd, condition.getSlopTankAtd());
+    }
+
+    private void setEditText(EditText editText, Object content) {
+        if (content != null) {
+            if (content instanceof Float && (Float)content == 0) {
+                Log.w("content", String.valueOf(content));
+            } else {
+                editText.setText(String.valueOf(content));
+            }
+        }
+    }
+
+    private void getCurrentData() {
+        Calendar cal = Calendar.getInstance();
+        Timestamp time = new Timestamp(cal.getTimeInMillis());
+        condition = new ShipCondition(
+                "Description draft",
+                time,
+                time,
+                null,
+                123,
+                456,
+                789,
+                0,
+                null,
+                time,
+                "Repl Location",
+                112,
+                131,
+                415,
+                167
+        );
+    }
+
+    private boolean currentDataExist() {
+        return true;
+    }
+
+    private void setDateAndTimeButton(Button dateButton, Button timeButton, Timestamp time) {
+        if (time != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(time.getTime());
+
+            if (dateButton != null) {
+                DateFormatSymbols symbols = new DateFormatSymbols();
+                String text = symbols.getMonths()[cal.get(Calendar.MONTH)] + " " + String.valueOf(cal.get(Calendar.YEAR));
+                dateButton.setText(text);
+            }
+
+            if (timeButton != null) {
+                Date date = new Date(cal.getTimeInMillis());
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.getDefault());
+                timeButton.setText(format.format(date));
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/pertamina/pertaminatuban/marine/models/ShipCondition.java
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/models/ShipCondition.java
@@ -18,4 +18,96 @@ public class ShipCondition {
     private float bunkerConsumptionSeatime;
     private float slopTankAta;
     private float slopTankAtd;
+
+    public ShipCondition(String descriptionDraft,
+                         Timestamp actualTimeArrival,
+                         Timestamp actualTimeDeparture,
+                         String gradeBunker,
+                         float robLastPort,
+                         float robAta,
+                         float robAtd,
+                         float repl,
+                         Timestamp comRepl,
+                         Timestamp compRepl,
+                         String replLocation,
+                         float bunkerConsumptionPort,
+                         float bunkerConsumptionSeatime,
+                         float slopTankAta,
+                         float slopTankAtd) {
+        this.descriptionDraft = descriptionDraft;
+        this.actualTimeArrival = actualTimeArrival;
+        this.actualTimeDeparture = actualTimeDeparture;
+        this.gradeBunker = gradeBunker;
+        this.robLastPort = robLastPort;
+        this.robAta = robAta;
+        this.robAtd = robAtd;
+        this.repl = repl;
+        this.comRepl = comRepl;
+        this.compRepl = compRepl;
+        this.replLocation = replLocation;
+        this.bunkerConsumptionPort = bunkerConsumptionPort;
+        this.bunkerConsumptionSeatime = bunkerConsumptionSeatime;
+        this.slopTankAta = slopTankAta;
+        this.slopTankAtd = slopTankAtd;
+    }
+
+    public String getDescriptionDraft() {
+        return descriptionDraft;
+    }
+
+    public Timestamp getActualTimeArrival() {
+        return actualTimeArrival;
+    }
+
+    public Timestamp getActualTimeDeparture() {
+        return actualTimeDeparture;
+    }
+
+    public String getGradeBunker() {
+        return gradeBunker;
+    }
+
+    public float getRobLastPort() {
+        return robLastPort;
+    }
+
+    public float getRobAta() {
+        return robAta;
+    }
+
+    public float getRobAtd() {
+        return robAtd;
+    }
+
+    public float getRepl() {
+        return repl;
+    }
+
+    public Timestamp getComRepl() {
+        return comRepl;
+    }
+
+    public Timestamp getCompRepl() {
+        return compRepl;
+    }
+
+    public String getReplLocation() {
+        return replLocation;
+    }
+
+    public float getBunkerConsumptionPort() {
+        return bunkerConsumptionPort;
+    }
+
+    public float getBunkerConsumptionSeatime() {
+        return bunkerConsumptionSeatime;
+    }
+
+    public float getSlopTankAta() {
+        return slopTankAta;
+    }
+
+    public float getSlopTankAtd() {
+        return slopTankAtd;
+    }
 }
