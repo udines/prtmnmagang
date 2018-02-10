e9d8b9dbd0004f89f1f9351a3e1c4787f61e092c
Udine <udines@users.noreply.github.com>
2018-02-10 13:36:26 +0700

    Tambah Model Untuk Input Marine
    model disesuaikan dengan database sebagai post body

---
+++ app/src/main/java/com/pertamina/pertaminatuban/marine/input/MarineInput.java
@@ -0,0 +1,88 @@
+package com.pertamina.pertaminatuban.marine.input;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class MarineInput {
+
+    @SerializedName("date")
+    @Expose
+    private String date;
+
+    @SerializedName("variable")
+    @Expose
+    private String variable;
+
+    @SerializedName("kapal_name")
+    @Expose
+    private String kapal;
+
+    @SerializedName("period")
+    @Expose
+    private String periode;
+
+    @SerializedName("bulan")
+    @Expose
+    private String bulan;
+
+    @SerializedName("callTanker")
+    @Expose
+    private String callTanker;
+
+    public MarineInput(String date, String variable, String kapal, String periode, String bulan, String callTanker) {
+        this.date = date;
+        this.variable = variable;
+        this.kapal = kapal;
+        this.periode = periode;
+        this.bulan = bulan;
+        this.callTanker = callTanker;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+
+    public String getKapal() {
+        return kapal;
+    }
+
+    public void setKapal(String kapal) {
+        this.kapal = kapal;
+    }
+
+    public String getPeriode() {
+        return periode;
+    }
+
+    public void setPeriode(String periode) {
+        this.periode = periode;
+    }
+
+    public String getBulan() {
+        return bulan;
+    }
+
+    public void setBulan(String bulan) {
+        this.bulan = bulan;
+    }
+
+    public String getCallTanker() {
+        return callTanker;
+    }
+
+    public void setCallTanker(String callTanker) {
+        this.callTanker = callTanker;
+    }
+}
