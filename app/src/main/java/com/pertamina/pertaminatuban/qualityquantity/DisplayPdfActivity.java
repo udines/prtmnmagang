931d8edfd7b2eea1d75361f20a8069b369969ee5
Udine <udines@users.noreply.github.com>
2018-02-22 19:49:46 +0700

    Display PDF via WebView
    load google docs url to load pdf

--- app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/DisplayPdfActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/DisplayPdfActivity.java
@@ -1,11 +1,17 @@
 package com.pertamina.pertaminatuban.qualityquantity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.pertamina.pertaminatuban.R;
 
@@ -30,6 +36,27 @@ public class DisplayPdfActivity extends AppCompatActivity {
 
     private void displayPdf() {
         String url = getIntent().getStringExtra("url");
+        WebView mWebview = findViewById(R.id.display_pdf_webview);
+        mWebview.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        mWebview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                activity.setTitle("Loading...");
+                activity.setProgress(progress * 100);
+                if(progress == 100)
+                    activity.setTitle(R.string.app_name);
+            }
+        });
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mWebview.loadUrl("http://docs.google.com/gview?embedded=true&url=" + url);
+        Log.w("pdf url", url);
     }
 
 }
