08f780727dee00647623ce0861749644125be296
Udine <udines@users.noreply.github.com>
2018-02-22 18:18:41 +0700

    Add Display PDF Activity

---
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/DisplayPdfActivity.java
@@ -0,0 +1,29 @@
+package com.pertamina.pertaminatuban.qualityquantity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.pertamina.pertaminatuban.R;
+
+public class DisplayPdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_pdf);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/harian/TestReportAdapter.java
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/harian/TestReportAdapter.java
@@ -1,11 +1,13 @@
 package com.pertamina.pertaminatuban.qualityquantity.harian;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pertamina.pertaminatuban.R;
+import com.pertamina.pertaminatuban.qualityquantity.DisplayPdfActivity;
 
 import java.util.ArrayList;
 
@@ -19,7 +21,7 @@ public class TestReportAdapter extends RecyclerView.Adapter<TestReportViewHolder
     }
 
     @Override
-    public TestReportViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TestReportViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_test_report, parent, false);
         final TestReportViewHolder holder = new TestReportViewHolder(view);
         holder.card.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +29,9 @@ public class TestReportAdapter extends RecyclerView.Adapter<TestReportViewHolder
             public void onClick(View view) {
                 ItemTestReport report = testReports.get(holder.getAdapterPosition());
                 //masuk ke halaman display pdf yang dipilih
+                Intent intent = new Intent(parent.getContext(), DisplayPdfActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                parent.getContext().startActivity(intent);
             }
         });
         return holder;
