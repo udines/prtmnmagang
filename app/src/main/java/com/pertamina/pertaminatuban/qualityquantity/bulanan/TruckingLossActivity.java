742df5117bad6e88c260e40e9c8f538232759341
Udine <udines@users.noreply.github.com>
2018-02-22 20:06:51 +0700

    Display PDF Trucking Loss
    terdapat anomali pdf tidak mau ngeload. penyebab belum diketahui.

--- app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/bulanan/TruckingLossActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/bulanan/TruckingLossActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.pertamina.pertaminatuban.R;
+import com.pertamina.pertaminatuban.qualityquantity.harian.ItemTestReport;
+import com.pertamina.pertaminatuban.qualityquantity.harian.TestReportAdapter;
+
+import java.util.ArrayList;
 
 public class TruckingLossActivity extends AppCompatActivity {
 
@@ -17,16 +23,33 @@ public class TruckingLossActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trucking_loss);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finish();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        displaySample();
     }
 
+    private void displaySample() {
+        RecyclerView recyclerView = findViewById(R.id.trucking_loss_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        ArrayList<ItemTestReport> testReports = new ArrayList<>();
+        testReports.add(new ItemTestReport(
+                "Januari 2018",
+                "https://firebasestorage.googleapis.com/v0/b/click-tuban.appspot.com/o/Laporan%20Trucking%20Loss%20MT%20Januari%202018.pdf?alt=media&token=11d0c3ef-257e-4f09-aabf-25fedd042c46"
+        ));
+        testReports.add(new ItemTestReport(
+                "Desember 2017",
+                "https://firebasestorage.googleapis.com/v0/b/click-tuban.appspot.com/o/Laporan%20Trucking%20Loss%20MT%20Januari%202018.pdf?alt=media&token=11d0c3ef-257e-4f09-aabf-25fedd042c46"
+        ));
+        testReports.add(new ItemTestReport(
+                "November 2017",
+                "https://firebasestorage.googleapis.com/v0/b/click-tuban.appspot.com/o/Laporan%20Trucking%20Loss%20MT%20Januari%202018.pdf?alt=media&token=11d0c3ef-257e-4f09-aabf-25fedd042c46"
+        ));
+        recyclerView.setAdapter(new TestReportAdapter(testReports));
+    }
 }
