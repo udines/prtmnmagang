1582c88dce5e03459bc1bcef0ce19893688ab44b
Udine <udines@users.noreply.github.com>
2018-02-22 18:16:02 +0700

    Update Foto Sample Adapter with Glide

--- app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/harian/FotoSampleAdapter.java
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/harian/FotoSampleAdapter.java
@@ -1,10 +1,12 @@
 package com.pertamina.pertaminatuban.qualityquantity.harian;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.pertamina.pertaminatuban.R;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 public class FotoSampleAdapter extends RecyclerView.Adapter<FotoSampleViewHolder> {
 
     private ArrayList<ItemFotoSample> fotoSamples;
+    private Context context;
 
     public FotoSampleAdapter(ArrayList<ItemFotoSample> fotoSamples) {
         this.fotoSamples = fotoSamples;
@@ -28,6 +31,9 @@ public class FotoSampleAdapter extends RecyclerView.Adapter<FotoSampleViewHolder
     public void onBindViewHolder(FotoSampleViewHolder holder, int position) {
         holder.date.setText(fotoSamples.get(position).getDate());
         holder.info.setText(fotoSamples.get(position).getInfo());
+        Glide.with(context)
+                .load(fotoSamples.get(position).getUrl())
+                .into(holder.image);
     }
 
     @Override
