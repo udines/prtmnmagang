c0e3e6428a6d731c1822477ba0f542742d4ea4b3
Udine <udines@users.noreply.github.com>
2018-02-23 10:45:27 +0700

    Tambah Input Working Loss

--- app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/bulanan/InputWorkingLossActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/qualityquantity/bulanan/InputWorkingLossActivity.java
@@ -1,22 +1,20 @@
 package com.pertamina.pertaminatuban.qualityquantity.bulanan;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 import com.pertamina.pertaminatuban.R;
 
-public class WorkingLossActivity extends AppCompatActivity {
+public class InputWorkingLossActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_working_loss);
+        setContentView(R.layout.activity_input_working_loss);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -26,20 +24,6 @@ public class WorkingLossActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        handleInputButton();
-    }
-
-    private void handleInputButton() {
-        Button button = findViewById(R.id.working_loss_input_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), InputWorkingLossActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
     }
 
 }
