0d92fb9549fb0e75a8d8645d37d4bc0412830172
Udine <udines@users.noreply.github.com>
2018-02-28 15:53:30 +0700

    Tambah Layout Untuk Tambah Rincian Perjalanan Dinas

--- app/src/main/java/com/pertamina/pertaminatuban/finance/perjalanan_dinas/TambahRincianActivity.java
+++ app/src/main/java/com/pertamina/pertaminatuban/finance/perjalanan_dinas/TambahRincianActivity.java
@@ -6,8 +6,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.pertamina.pertaminatuban.R;
@@ -17,7 +19,9 @@ import java.util.ArrayList;
 public class TambahRincianActivity extends AppCompatActivity {
 
     private Spinner spinnerJenis;
-    private AutoCompleteTextView antarKotaDari, antarKotaKe;
+
+    private LinearLayout containerAntarKota, containerTiketPesawat, containerUangHarian, containerUangMakan,
+    containerTransport, containerAkomodasiLaundry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +36,74 @@ public class TambahRincianActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        spinnerJenis = findViewById(R.id.tambah_rincian_jenis);
-        antarKotaDari = findViewById(R.id.tambah_rincian_antar_kota_dari);
-        antarKotaKe = findViewById(R.id.tambah_rincian_antar_kota_ke);
 
+        containerUangHarian = findViewById(R.id.tambah_rincian_container_uang_harian);
+        containerUangMakan = findViewById(R.id.tambah_rincian_container_uang_makan_harian);
+        containerAntarKota = findViewById(R.id.tambah_rincian_container_antar_kota);
+        containerTiketPesawat = findViewById(R.id.tambah_rincian_container_tiket_pesawat);
+        containerTransport = findViewById(R.id.tambah_rincian_container_transport);
+        containerAkomodasiLaundry = findViewById(R.id.tambah_rincian_container_akomodasi_laundry);
+
+        handleSpinnerJenis();
+    }
+
+    private void clearContainer() {
+        containerUangHarian.setVisibility(View.GONE);
+        containerUangMakan.setVisibility(View.GONE);
+        containerAntarKota.setVisibility(View.GONE);
+        containerTiketPesawat.setVisibility(View.GONE);
+        containerTransport.setVisibility(View.GONE);
+        containerAkomodasiLaundry.setVisibility(View.GONE);
+    }
+
+    private void handleSpinnerJenis() {
+        spinnerJenis = findViewById(R.id.tambah_rincian_jenis);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.jenis_claim_trip, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerJenis.setAdapter(adapter);
+        spinnerJenis.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        clearContainer();
+                        containerAntarKota.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        clearContainer();
+                        containerTransport.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        clearContainer();
+                        containerAkomodasiLaundry.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        clearContainer();
+                        containerUangMakan.setVisibility(View.VISIBLE);
+                        break;
+                    case 4:
+                        clearContainer();
+                        break;
+                    case 5:
+                        clearContainer();
+                        containerUangHarian.setVisibility(View.VISIBLE);
+                        break;
+                    case 6:
+                        clearContainer();
+                        containerTiketPesawat.setVisibility(View.VISIBLE);
+                        break;
+                    case 7:
+                        clearContainer();
+                        break;
+                }
+            }
 
-        ArrayList<String> dariDanKe = new ArrayList<>();
-        dariDanKe.add("Tuban");
-        dariDanKe.add("Surabaya");
-        dariDanKe.add("Yogyakarta");
-        dariDanKe.add("Semarang");
-        dariDanKe.add("Jakarta");
-        ArrayAdapter<String> adapterDari = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,dariDanKe);
-        antarKotaDari.setAdapter(adapterDari);
-        antarKotaKe.setAdapter(adapterDari);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
 }
